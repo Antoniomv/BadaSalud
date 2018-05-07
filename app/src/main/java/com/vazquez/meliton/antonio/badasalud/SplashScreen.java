5a6a7278956509f712285445710c3cf11fdbef18
Antonio <amelitonvazquez@gmail.com>
2018-05-07 22:30:35 +0200

    SplashScreen inicial, con logo y rotación

--- app/src/main/java/com/vazquez/meliton/antonio/badasalud/SplashScreen.java
+++ app/src/main/java/com/vazquez/meliton/antonio/badasalud/SplashScreen.java
@@ -3,18 +3,25 @@ package com.vazquez.meliton.antonio.badasalud;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 //extendemos a Activity para poder usar otros themes en el manifest
 public class SplashScreen extends Activity {
+
+    Animation rotacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //CREAMOS UN HANDLER PARA INDICARLE EL TIEMPO QUE QUEREMOS QUE DURE ANTES
+        //INICIO LA ROTACIÓN DE LA IMAGEN
+        rotarImagen();
+
+        //CREAO UN HANDLER PARA INDICARLE EL TIEMPO QUE QUEREMOS QUE DURE ANTES
         //ANTES DE LLEVARNOS AL SIGUIENTE ACTIVITY
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -25,6 +32,13 @@ public class SplashScreen extends Activity {
                 //usamos finish para evitar volver a la pantalla de carga
                 finish();
             }
-        },3000);
+        },1550);
+    }
+
+    //MÉTODO ROTACIÓN IMAGEN INICIAL
+    private void rotarImagen() {
+        ImageView imagen = findViewById(R.id.imagenRotar);
+        rotacion = AnimationUtils.loadAnimation(this, R.anim.rotar);
+        imagen.setAnimation(rotacion);
     }
 }
