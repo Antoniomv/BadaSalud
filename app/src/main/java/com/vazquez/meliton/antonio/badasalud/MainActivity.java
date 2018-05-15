10d91c67f3748819e1187cb75c37b7cb25fa341f
Antonio <amelitonvazquez@gmail.com>
2018-05-15 17:49:04 +0200

    Reestructuración

--- app/src/main/java/com/vazquez/meliton/antonio/badasalud/MainActivity.java
+++ app/src/main/java/com/vazquez/meliton/antonio/badasalud/MainActivity.java
@@ -1,14 +1,36 @@
 package com.vazquez.meliton.antonio.badasalud;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.vazquez.meliton.antonio.badasalud.Fragmentos.RegistroFragmento;
+
+public class MainActivity extends AppCompatActivity implements RegistroFragmento.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            cargarInicio();
+        }
+    }
+
+    private void cargarInicio() {
+        FragmentManager fragmentManager =  getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        RegistroFragmento registroFragmento = new RegistroFragmento();
+        fragmentTransaction.add(R.id.fragment_container, registroFragmento);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
