11e8dc737c207f887a5bd35206efad1c7edb8de0
Antonio <amelitonvazquez@gmail.com>
2018-05-18 18:42:57 +0200

    Volley Singleton

---
+++ app/src/main/java/com/vazquez/meliton/antonio/badasalud/Constantes/VolleySingleton.java
@@ -0,0 +1,36 @@
+package com.vazquez.meliton.antonio.badasalud.Constantes;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public final class VolleySingleton {
+    private static VolleySingleton singleton;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private VolleySingleton(Context context) {
+        VolleySingleton.context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (singleton == null) {
+            singleton = new VolleySingleton(context.getApplicationContext());
+        }
+        return singleton;
+    }
+
+    public RequestQueue getRequestQueue() { //Obtiene la instancia de la cola de peticiones
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) { //Añade petición a la cola
+        getRequestQueue().add(req);
+    }
+}
