36335e81e487e5352b0b26d8f4557b0ead6a5d64
Antonio <amelitonvazquez@gmail.com>
2018-06-06 11:33:22 +0200

    ADD: RecyclerView Hospital v2.0

--- app/src/main/java/com/vazquez/meliton/antonio/badasalud/fragmentos/ListaHospitalFragment.java
+++ app/src/main/java/com/vazquez/meliton/antonio/badasalud/fragmentos/ListaHospitalFragment.java
@@ -144,9 +144,9 @@ public class ListaHospitalFragment extends Fragment implements Response.Listener
                 JSONObject jsonObject=null;
                 jsonObject=json.getJSONObject(i);
 
-                hospital.setNombre(jsonObject.optString("nombre"));
-                hospital.setDireccion(jsonObject.optString("direccion"));
-                hospital.setTelefono(jsonObject.optInt("telefono"));
+                hospital.setNombre(jsonObject.optString("NOMBRE"));
+                hospital.setDireccion(jsonObject.optString("DIRECCION"));
+                hospital.setTelefono(jsonObject.optInt("TELEFONO"));
                 listaHospitales.add(hospital);
             }
 
